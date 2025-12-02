f8ea83650c0057ae155235e10a9883d7cfe780d1
anylots <22675649+anylots@users.noreply.github.com>
2025-12-02 10:29:45 +0800

    dev

--- crates/precompile/src/lib.rs
+++ crates/precompile/src/lib.rs
@@ -255,7 +255,7 @@ impl Precompiles {
         static INSTANCE: OnceBox<Precompiles> = OnceBox::new();
         INSTANCE.get_or_init(|| {
             let mut precompiles = Self::viridian().clone();
-            precompiles.extend(bls12_381::precompiles()); // add BLS12-381 precompiles
+            // precompiles.extend(bls12_381::precompiles()); // add BLS12-381 precompiles
             precompiles.extend([
                 modexp::EMERALD, // 0x05
             ]);
